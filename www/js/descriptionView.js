9dd4fa94ec9b720149dc51c72c1f342ab78759a1
hermwong <herm.wong@gmail.com>
2015-06-08 13:45:26 -0700

    [#9] created basic hello world example using react.js

---
+++ www/js/descriptionView.js
@@ -0,0 +1,4 @@
+React.render(
+    <span>using React with JSX transformer</span>,
+    document.getElementById('description')
+);
---
+++ www/js/titleView.js
@@ -0,0 +1,4 @@
+React.render(
+    <h1>PhoneGap Desktop using React</h1>,
+    document.getElementById('title')
+);
