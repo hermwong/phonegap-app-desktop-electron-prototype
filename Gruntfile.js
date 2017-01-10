f318fec2dd5c3b3d48a5ff47c6191c0836e0cbb2
Herm Wong <herm.wong@gmail.com>
2017-01-10 09:59:42 -0800

    get the electron template working with Electron 1.4.13

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,166 @@
+var execSync = require('child_process').execSync;
+var shell = require('shelljs');
+
+var APPVERSION = '5.0.0';
+var ELECTRONVERSION = '1.4.13';
+var isRelease = (process.argv[2] === 'release');
+
 module.exports = function(grunt) {
+    var osxArchive = './installers/osx64/PhoneGap-Desktop-Beta-' + APPVERSION + '-mac.zip';
+    var winArchive = './installers/win32/PhoneGap-Desktop-Beta-' + APPVERSION + '-win.zip';
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        'build-electron-app': {
-            options: {
-                platforms: ["darwin", "win32"],
-                app_dir: './www',
-                build_dir: './build'
+        'electron': {
+            osxBuild: {
+                options: {
+                    name: 'PhoneGap',
+                    dir: './www',
+                    out: './build',
+                    version: ELECTRONVERSION,
+                    platform: 'darwin',
+                    arch: 'x64',
+                    icon: './www/img/app-icons/icon.icns',
+                    asar: false
+                }
+            },
+            winBuild: {
+                options: {
+                    name: 'PhoneGap',
+                    dir: './www',
+                    out: './build',
+                    version: ELECTRONVERSION,
+                    platform: 'win32',
+                    arch: 'ia32',
+                    icon: './www/img/app-icons/icon.ico',
+                    asar: { unpackDir:'{bin,node_modules/adm-zip,node_modules/adm-zip/**}' }
+                }
             }
         }
     });
 
     // Load the grunt plugins.
+    //grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-electron-app-builder');
+    grunt.loadNpmTasks('grunt-electron');
 
     // Register the task to install dependencies.
     grunt.task.registerTask('install-dependencies', function() {
-        var exec = require('child_process').exec,
-        callback = this.async();
+        //execSync('node src/js/download-node.js');
 
-        exec('npm install --production', { cwd: './www' }, function(e, stdout, stderr) {
-            console.log(stdout);
-            callback();
-        });
+        var npmInstall = 'npm install';
+        if (isRelease) {
+            npmInstall += ' --production';
+        }
+
+        execSync(npmInstall, { cwd: './www' });
+
+        // npm/phonegap workarounds are for Windows, Mac can install normally
+        if (process.platform === 'darwin' && isRelease) {
+            var config = JSON.parse(grunt.file.read('package.json'));
+            var pgVersion = config.devDependencies.phonegap;
+            execSync('npm install npm@3.10.3 phonegap@' + pgVersion, { cwd: './www' });
+        }
     });
 
-    // Remove build directories
-    grunt.task.registerTask('clean-build-dir', function() {
-        var shell = require('shelljs');
-        shell.rm('-rf', './build/darwin');
-        shell.rm('-rf', './build/win32');
+    // Clean old files
+    grunt.task.registerTask('clean-old-files', function() {
+        // clean build directories
+        shell.rm('-rf', './build');
+
+        // if we are using debugMac/debugWin commands, don't remove files
+        if (process.argv[2] && process.argv[2].includes('debug'))
+            return;
+
+        // clean node binary
+        var nodePath = './www/bin/node';
+        shell.rm('-rf', (process.platform === 'darwin') ? nodePath : (nodePath + '.exe'));
+
+        // clean node dependencies
+        shell.rm('-rf', './www/node_modules');
+
+        // clean installers directory for release builds
+        if (isRelease) {
+            shell.rm('-rf', './installers');
+        }
     });
 
-    // Rename app from Electron to PhoneGap
-    grunt.task.registerTask('rename-app', function() {
-        var fs = require('fs');
-        fs.rename('./build/darwin/Electron.app', './build/darwin/PhoneGap.app');
-        fs.rename('./build/win32/electron.exe', './build/win32/PhoneGap.exe');
+    // start the local server for update checker
+    grunt.task.registerTask('start-localhost', function() {
+        shell.exec('node ./update-server/server.js');
+    });
+
+    // create the OSX DMG installer
+    grunt.task.registerTask('osx-installer', function() {
+        if (process.platform === 'darwin' && isRelease) {
+            // OSX code signing
+            var signConfig = grunt.file.readJSON("sign-config.json");
+            shell.exec("codesign --verbose --deep --force --sign " + "'"+signConfig.certName+"'" + " build/PhoneGap-darwin-x64/PhoneGap.app");
+            shell.exec("codesign --verbose --verify build/PhoneGap-darwin-x64/PhoneGap.app");
+            shell.exec("codesign -vv -d build/PhoneGap-darwin-x64/PhoneGap.app");
+            shell.exec("codesign --verbose --force --sign " + "'"+signConfig.certName+"'" + " build/PhoneGap-darwin-x64/PhoneGap.app/Contents/MacOS/PhoneGap");
+            shell.exec("codesign --verbose --verify build/PhoneGap-darwin-x64/PhoneGap.app/Contents/MacOS/PhoneGap");
+
+            shell.exec('bash ./res/installers/osx/package-pgd.sh');
+        }
+    });
+
+    grunt.task.registerTask('copy-package-json', function() {
+        if (isRelease) {
+            // disable the dev console before copying the package.json to www folder
+            var config = grunt.file.read('./src/config/package.json');
+            var releaseConfig = config.replace('"devTools" : true', '"devTools" : false');
+            grunt.file.write('./www/package.json', releaseConfig);
+        } else {
+            // copy package.json to www folder
+            grunt.file.copy('./src/config/package.json', './www/package.json');
+        }
+    });
+
+    // copy the EULA into the installer folders
+    grunt.task.registerTask('copy-eula', function() {
+        grunt.file.copy('./src/license.txt', './res/installers/osx/license.txt');
+        grunt.file.copy('./src/license.txt', './res/installers/win/license.txt');
     });
 
     // Open the built app
     grunt.task.registerTask('open', function() {
-        var os = require('os'),
-            fs = require('fs'),
-            opener = require('opener'),
-            appName = JSON.parse(fs.readFileSync('./www/package.json')).name,
-            macPath = 'build/darwin/appName.app',
-            winPath = 'build/win32/appName.exe';
-
-            macPath = macPath.replace(/appName/g, appName);
-            winPath = winPath.replace(/appName/g, appName);
-
-            opener((os.platform() === 'darwin') ? macPath : winPath);
+        // disabling on Windows for now, will re-evaluate in the future
+        var opener = require('opener'),
+            macPath = 'build/PhoneGap-darwin-x64/PhoneGap.app';
+            opener(macPath);
     });
 
-    grunt.registerTask('default', ['install-dependencies', 'clean-build-dir', 'build-electron-app', 'rename-app', 'open']);
+    // default - runs the dev build
+    grunt.registerTask(
+        'default',
+        [
+            'clean-old-files',
+            //'copy-package-json',
+            'install-dependencies',
+            //'copy-eula',
+            'electron:' + (process.platform === 'darwin' ? 'osxBuild' : 'winBuild'),
+            'open'//,
+            //'start-localhost'
+        ]
+    );
 
+    // production build task
+    grunt.registerTask(
+        'release',
+        [
+            'clean-old-files',
+            'copy-package-json',
+            'install-dependencies',
+            'copy-eula',
+            'electron:' + (process.platform === 'darwin' ? 'osxBuild' : 'winBuild'),
+            'osx-installer',
+            'open'
+        ]
+    );
+
+    grunt.registerTask('debugMac',['clean-old-files','electron:osxBuild','open']);
+    grunt.registerTask('debugWin',['clean-old-files','electron:winBuild','open']);
 };
--- www/index.js
+++ www/index.js
@@ -1,39 +1,81 @@
 'use strict';
 
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
 
-// Report crashes to our server.
-require('crash-reporter').start();
+const path = require('path')
+const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the javascript object is GCed.
-var mainWindow = null;
+let mainWindow;
+let debugMode;
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  if (process.platform != 'darwin')
-    app.quit();
-});
+function createWindow () {
+    // Create the browser window.
+    if (process.platform != 'win32') {
+        mainWindow = new BrowserWindow({width: 450, height: 622, resizable: false, title: 'PhoneGap', center: true});
+    } else {
+        mainWindow = new BrowserWindow({width: 463, height: 656, resizable: false, title: 'PhoneGap', center: true});
+    }
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    var fs = require('fs');
+    var pathToPackageJSONFile = __dirname + "/package.json";
+    fs.readFile(pathToPackageJSONFile, 'utf8', function(err, data) {
+        if (err) {
+            mainWindow.webContents.executeJavaScript('console.log("not found");');
+        } else {
+            mainWindow.webContents.executeJavaScript('console.log("found");');
+            var obj = JSON.parse(data);
+            debugMode = obj.window.devTools;
+
+            if (debugMode) {
+                // Open the devtools.
+                mainWindow.webContents.openDevTools();
+            }
+        }
+    });
+
+    mainWindow.webContents.on('did-finish-load', function() {
+        // make sure the debug window is set properly
+        mainWindow.webContents.executeJavaScript('console.log("debugMode: '+ debugMode +' ");');
+        mainWindow.webContents.executeJavaScript('setDebugFlag('+ debugMode +');');
+    });
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+        app.quit();
+    })
+}
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
-app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+app.on('ready', createWindow);
 
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-  // Open the devtools.
-  mainWindow.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin')
     app.quit();
-  });
 });
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
+})
